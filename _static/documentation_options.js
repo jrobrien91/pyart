c67da6f96904f8e62db08542ef4ecfa18e34380d
jrobrien91 <jrobrien91@users.noreply.github.com>
2022-03-10 21:59:12 +0000

    deploy: 27a8022e1eca4a23b134975b15be980ea70d063f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.7.dev+eef9c26',
+    VERSION: '1.11.7.dev+27a8022',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'dirhtml',
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
